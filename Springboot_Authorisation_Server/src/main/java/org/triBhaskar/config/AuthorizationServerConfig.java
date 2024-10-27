b9f88e0332a5af78209b8e9e5a18c8583d06c9e5
TriBhaskar <panthribhaskar@gmail.com>
2024-10-27 21:53:18 +0530

    chore: Authorisation server configurations

--- Springboot_Authorisation_Server/src/main/java/org/triBhaskar/config/AuthorizationServerConfig.java
+++ Springboot_Authorisation_Server/src/main/java/org/triBhaskar/config/AuthorizationServerConfig.java
@@ -2,12 +2,16 @@ package org.triBhaskar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.UUID;
@@ -18,17 +22,34 @@ public class AuthorizationServerConfig {
          @Bean
          public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
              OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-             return http.build();
+             return http
+                     .formLogin(Customizer.withDefaults())
+                     .build();
          }
 
-         @Bean
-         public RegisteredClientRepository registeredClientRepository() {
-             RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString())
-                     .clientId("client")
-                     .clientSecret("{noop}secret")
-                     .scope("api.read")
-                     .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                     .build();
-             return new InMemoryRegisteredClientRepository(client);
-         }
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://localhost:9001/login/oauth2/code/client-oidc") // Updated to match Postman's redirect URI
+                .scope("openid")
+                .scope("api.read")
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+//                        .requireProofKey(true) // Enable PKCE
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(client);
+    }
+//    @Bean
+//    public AuthorizationServerSettings authorizationServerSettings() {
+//        return AuthorizationServerSettings.builder()
+//                .issuer("http://localhost:9001")
+//                .build();
+//    }
 }
---
+++ Springboot_Authorisation_Server/src/main/java/org/triBhaskar/controller/IndexController.java
@@ -0,0 +1,12 @@
+package org.triBhaskar.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome to authorisation server";
+    }
+}
