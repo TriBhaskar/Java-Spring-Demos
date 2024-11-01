5a6cee2bf59478592fe9a556b6c45f6f6f99f8b4
TriBhaskar <panthribhaskar@gmail.com>
2024-11-01 12:37:24 +0530

    chore: Okta config added

---
+++ Springboot_Oauth2_Okta/src/main/java/org/triBhaskar/controller/HomeController.java
@@ -0,0 +1,20 @@
+package org.triBhaskar.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "Login!!Welcome Home";
+    }
+    @GetMapping("/logout")
+    public String logout(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "Logout";
+    }
+}
